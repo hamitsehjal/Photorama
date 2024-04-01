90def45754992268b6eddaccf67a4b3b53b9fe4f
Hamit Sehjal <hamitsehjal26@gmail.com>
2024-04-01 17:48:37 -0400

    Configure the project

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Hamit Sehjal on 2024-04-01.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    @IBOutlet private var imageView:UIImageView!
+
+
+}
+
